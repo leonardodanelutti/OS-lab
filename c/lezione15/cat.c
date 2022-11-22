ab6cf2d0e91a78d3a05a61c2ea8021222c677174
leonardo <leodane2002@gmail.com>
2022-11-22 10:34:34 +0100

    New solution.

--- c/lezione15/cat.c
+++ c/lezione15/cat.c
@@ -1,43 +1,38 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+  if(argc < 2) {
+    fprintf(stderr, "Specificare il nome di un file\n");
+    return 1;
+  }
 
-	if (argc != 2) {
-		printf("Utilizzo del comando: %s <file>", argv[0]);
-		return 1;
-	}
-	
-	char *filename = argv[1];
-	
-	int file = open(filename, O_RDONLY);
-	
-	if(!file) {
-		fprintf(stderr, "Errore nell’apertura del file!\n");
-		return 2;
-	}
-	
-	int pipes[2] = { };
-	if(pipe(pipes) == -1) {
-		perror("pipe()");
-		return 1;
-  	}
-  	
-  	pid_t ls = fork();
-  	switch(ls) {
-    		case -1:
-      			perror("fork()");
-      			exit(2);
-    		case 0: // figlio
-      			dup2(file, 1); // redirigo stdout
-      			close(pipes[1]);
-      			execlp("ls", "ls", "-l", NULL);
-      			perror("ls"); // se arrivo qui, execlp() ha fallito
-      			return 2;
-  	} // il padre prosegue
-  	
-  	
-	
+  int fd = open(argv[1], O_RDONLY);
+  if(fd == -1) {
+    fprintf(stderr, "Impossibile aprire il file %s: %s\n",
+            argv[1], strerror(errno));
+    return 2;
+  }
+
+  int pid = fork();
+  switch(pid) {
+    case -1:
+      perror("fork()");
+      return 3;
+    case 0: // figlio
+      dup2(fd, 0);
+      execlp("cat", "cat", NULL);
+      perror("Impossibile eseguire il comando cat");
+      return 4;
+    default:
+      wait(NULL);
+      close(fd);
+  }
+
+  return 0;
 }
